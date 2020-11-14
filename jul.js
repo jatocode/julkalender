c6ae3caca3ea882c7989f8faf516d5e99d69f112
Tobias Jansson <tobias.jansson@gmail.com>
2020-11-14 16:22:10 +0100

    Förberett för texter

--- jul.js
+++ jul.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", julenärhär);
 
-function julenärhär() {
-    let luckor = [...document.getElementsByClassName('clucka')];
+const texter = [
+    'God jul',
+    'Lorekjasdlkjh kjsahdflkjhas fdlkjhsa df',
+    'lklksljlasd'
+]
 
-    luckor.forEach(el => {
+function julenärhär() {
+    let luckor = [...document.getElementsByClassName('jul')];
+
+    luckor.forEach((el,i) => {
+        let fram = el.querySelector('.fram');
+        let bak = el.querySelector('.bak');
+        let input = el.querySelector('input');
+        fram.textContent = input.id;
+        bak.textContent = texter[i % texter.length];
         el.onclick = (e) => {
             if (e.target.id > new Date().getDate()) {
                 e.preventDefault();
                 e.stopPropagation();
             }
-
         }
     });
 }
